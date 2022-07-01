acc050ab203dfa1b1be6b0a96f49a75c080aad65
LIMDANBI <dlaeksql08222@gmail.com>
2022-07-01 17:24:14 +0900

    flask 실습

---
+++ flask/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5102)
+
+# debug 모드로 실행, 모든 IP 에서 접근 허용, 5102 포트로 사용하는 것을 의미
+
