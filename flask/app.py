e358c7f9da4e6da0805b364f66ba3eb9e21d88ec
danbibibi <55095806+LIMDANBI@users.noreply.github.com>
2022-07-01 17:35:24 +0900

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -10,9 +10,6 @@ def hello_world():
 def hello_mlops():
     return "<p>Hello, MLOps!</p>"
 
-
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5102)
-
-# debug 모드로 실행, 모든 IP 에서 접근 허용, 5102 포트로 사용하는 것을 의미
+    app.run(debug=True, host='0.0.0.0', port=5102) # debug 모드로 실행, 모든 IP 에서 접근 허용, 5102 포트로 사용하는 것을 의미
 
