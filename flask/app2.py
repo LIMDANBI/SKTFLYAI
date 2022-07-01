7817bb0875a57e4ab9dbcde37e37f4299293da0e
LIMDANBI <dlaeksql08222@gmail.com>
2022-07-01 17:45:18 +0900

    flask - post method

---
+++ flask/app2.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST", "PUT"])
+def inference():
+    return json.dumps({'hello': 'world'}), 200 # http status code 를 200 으로 반환하는 것을 의미
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5001)
+
+
