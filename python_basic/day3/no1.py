334813ff4ca3d53889f8f4a846293716d8a96653
danbibibi <dlaeksql08222@gmail.com>
2022-07-13 00:35:48 +0900

    day3 - no1

--- python_basic/day3/no1.py
+++ python_basic/day3/no1.py
@@ -1,8 +1,15 @@
-get_max = 0
+max_val = 0
+
+def get_max(a, b):
+    if a<b:
+        return b
+    else:
+         return a
+
 while True:
     num = int(input("양의 정수 입력: "))
     if num==0:
         print("프로그램을 종료합니다.")
         break
-    get_max = max(get_max, num)
-    print("현재까지 가장 큰 값: {}".format(get_max))
+    max_val = get_max(max_val, num)
+    print("현재까지 가장 큰 값: {}".format(max_val))
