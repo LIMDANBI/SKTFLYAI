225c0a85fb95dcbf1b6c23da4ff26928dc3f3711
LIMDANBI <dlaeksql08222@gmail.com>
2022-08-19 16:41:53 +0900

    face crop

--- jetsnonano/face_crop.py
+++ jetsnonano/face_crop.py
@@ -2,7 +2,7 @@ from PIL import Image
 import face_recognition
 
 image =	face_recognition.load_image_file('./me.jpg')
-face_locations =	face_recognition.face_locations(image)
+face_locations = face_recognition.face_locations(image)
 for face_location in face_locations:
     top, right,	bottom,	left = face_location
     face_image = image[top:bottom,	left:right]
